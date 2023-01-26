2bb25bc1171ad0755fbae1bda406da310105739e
Your Name <you@example.com>
2023-01-26 21:28:48 +0900

    asdas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'add-vacancy',
+    path: 'vacancy-add',
     loadChildren: () =>
       import('./add-vacancy/add-vacancy.module').then(
         (m) => m.AddVacancyModule
